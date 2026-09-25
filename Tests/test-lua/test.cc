c12f7bb91a0a6755556508afc753100c0da87867
xmz-team <ad-ios332@outlook.com>
2026-09-25 08:59:29 +0800

    updated by the update script; update Tests/test-lua/{test.cc, test.lua} ; 2026-09-25 08:59

--- Tests/test-lua/test.cc
+++ Tests/test-lua/test.cc
@@ -1,12 +1,18 @@
 #include <libxmz/log.hpp>
 #include <libxmz/fs.hpp>
+#include <libxmz/aux.hpp>
 #include <a1/core/mod/a1mod_luarun.hpp>
 
 a1mod::luatime lt;
 
 int main() {
-    lt.init();
-    lt.run_file(std::string(xmz::fs::pwd() + "/test.lua").c_str());
-    xmz::println("path:", xmz::fs::pwd());
-    return 0;
+    if (xmz::aux::is_file("./test.lua") == 0) {
+        lt.init();
+        lt.run_file(std::string(xmz::fs::pwd() + "/test.lua").c_str());
+        //xmz::println("path:", xmz::fs::pwd());
+        return 0;
+    } else {
+        xmz::log::error("test file: ./test.lua not exist");
+        return 1;
+    }
 }
--- Tests/test-lua/test.lua
+++ Tests/test-lua/test.lua
@@ -1,1 +1,13 @@
 print("hello")
+print("------------")
+print("High priority list")
+print(a1.GetHighPriorityList())
+print("------------")
+print("Low priority list")
+print(a1.GetLowPriorityList())
+print("------------")
+if (a1.GetProcessPid("a1") == -1) then
+    print("a1 is not running")
+else
+    print("a1 pid is " .. a1.GetProcessPid("a1"))
+end
