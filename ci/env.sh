352b2abb9934b8922bcd56779003322e8688fa07
xmz-team <xmz-team@outlook.com>
2026-09-26 22:15:36 +0800

    updated by the update script; Change cxxa1.ci.yml or build ci/* ; 2026-09-26 22:15

--- ci/env.sh
+++ ci/env.sh
@@ -18,7 +18,9 @@ if [ -z "$jb" ] && [ $(uname -s) = "Darwin" ] || [ $(uname -s) = "Linux" ]; then
     fi
     if [ $(uname -s) = "Darwin" ] && [ "$(dpkg --print-architecture)" = "darwin-arm64" ]; then
         CXXFLAGS1="\
-  -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/*/var/jb/usr/lib \
+  -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libzip5/var/jb/usr/lib \
+  -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libssl3/var/jb/usr/lib \
+  -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libcurl/var/jb/usr/lib \
   -I$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libcurl-openssl-dev/var/jb/usr/include \
   -I$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libzip-dev/var/jb/usr/include \
   -I$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libssl-dev/var/jb/usr/include"
