a38adffb2e71bed9b0a6359f77f08c352f052e21
xmz-team <xmz-team@outlook.com>
2026-09-27 00:18:55 +0800

    updated by the update script; Change cxxa1.ci.yml or build ci/* ; 2026-09-27 00:18

--- ci/env.sh
+++ ci/env.sh
@@ -23,6 +23,7 @@ if [ -z "$jb" ] && [ $(uname -s) = "Darwin" ] || [ $(uname -s) = "Linux" ]; then
   -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libssl-dev/var/jb/usr/lib \
   -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libssl3/var/jb/usr/lib \
   -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libcurl/var/jb/usr/lib \
+  -L$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libcurl-openssl-dev/var/jb/usr/lib \
   -I$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libcurl-openssl-dev/var/jb/usr/include \
   -I$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libzip-dev/var/jb/usr/include \
   -I$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../tmp/lib/libssl-dev/var/jb/usr/include"
