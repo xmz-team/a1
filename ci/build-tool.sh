64ee6b64d86b9b58ddb63a9a73790eab19d9daf5
xmz-team <xmz-team@outlook.com>
2026-09-26 19:55:34 +0800

    updated by the update script; Change cxxa1.ci.yml and build ci/* ; 2026-09-26 19:55

--- ci/build-tool.sh
+++ ci/build-tool.sh
@@ -5,10 +5,10 @@ build_tool() {
     source "${script_path}/env.sh"
     cd ${script_path}/../src/bin
     cd bundle
-    c++ $CXXFLAGS bundle.mm -o bundle && sign bin bundle && mv bundle ../../../
+    clang++ $CXXFLAGS bundle.mm -o bundle && sign bin bundle && mv bundle ../../../
     cd ../flock-ios
-    c++ $CXXFLAGS flock.cc -o flock && sign bin flock && mv flock ../../../
+    clang++ $CXXFLAGS flock.cc -o flock && sign bin flock && mv flock ../../../
     cd ../myini
-    c++ $CXXFLAGS myini.cc -o myini && sign bin myini && mv myini ../../../
+    clang++ $CXXFLAGS myini.cc -o myini && sign bin myini && mv myini ../../../
     cd "${script_path}/../"
 }
